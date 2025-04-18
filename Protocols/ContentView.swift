9022707d095bb79440b08931e50e6b8607634ee7
saurabhdutta047 <saurabhdutta047@gmail.com>
2025-04-18 11:46:08 +0530

    Initial Commit

---
+++ Protocols/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Protocols
+//
+//  Created by Saurabh Dutta on 18/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Protocols/ProtocolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolsApp.swift
+//  Protocols
+//
+//  Created by Saurabh Dutta on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
